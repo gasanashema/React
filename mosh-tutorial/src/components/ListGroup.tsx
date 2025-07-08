66432864f231ddf353a5fcd3f0d1f6dfd480e7ec
Shema-Philbert-Gasana <shemaphilbert8@gmail.com>
2025-07-08 20:01:47 +0930

    Managing state

--- mosh-tutorial/src/components/ListGroup.tsx
+++ mosh-tutorial/src/components/ListGroup.tsx
@@ -1,16 +1,15 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleClick = (event:MouseEvent)=>{console.log(event)}
+  let [selectedIndex,setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 && <p>No Items</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li className={selectedIndex === index? 'list-group-item active':'list-group-item'} key={item} onClick={()=>setSelectedIndex(index)}>
             {item}
           </li>
         ))}
