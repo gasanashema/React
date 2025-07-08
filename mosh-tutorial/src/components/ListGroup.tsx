5809ecc9d624033c60d16566189ce2bc10dd6c96
Shema-Philbert-Gasana <shemaphilbert8@gmail.com>
2025-07-08 10:40:18 +0200

    Conditional rendering

--- mosh-tutorial/src/components/ListGroup.tsx
+++ mosh-tutorial/src/components/ListGroup.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
   return (
     <>
       <h1>List Group</h1>
+      {items.length === 0 && <p>No Items</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
