1305f8a7f201c493c34141c9c6c18076eea36a08
Shema-Philbert-Gasana <shemaphilbert8@gmail.com>
2025-07-08 10:55:54 +0200

    Handling events

--- mosh-tutorial/src/components/ListGroup.tsx
+++ mosh-tutorial/src/components/ListGroup.tsx
@@ -1,15 +1,16 @@
-import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  const handleClick = (event:MouseEvent)=>{console.log(event)}
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 && <p>No Items</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item" key={item}>
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
