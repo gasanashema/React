d9769c0e43366612d104f196c4540e6883d7ed5f
Shema Philbert <shemaphilbert8@gmail.com>
2025-07-14 10:58:29 +0200

    created main component

---
+++ static-pages/src/components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main(){
+    return (
+
+        <>
+            <h1>Fun facts about React!</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </>
+    )
+}
