e780f6f330ce51305ad9b3c9b2a89952836a6c90
Shema Philbert <shemaphilbert8@gmail.com>
2025-07-23 10:26:17 +0200

    returned a cleanup funtion

--- small-projects/src/App.jsx
+++ small-projects/src/App.jsx
@@ -29,8 +29,8 @@ function App() {
 
       {/* <FormHandling /> */}
       {/* <Car /> */}
-      {/* <Timer /> */}
-        <CounterTitle />
+      <Timer />
+        {/* <CounterTitle /> */}
     </>
   )
 }
